d4125aa49c10883f11069c3ecb4be215bb8a7812
Clément Robert <cr52@protonmail.com>
2021-03-07 10:23:30 +0100

    better rich integration for ipython

---
+++ ipython/rich_init.py
@@ -0,0 +1,16 @@
+# install:
+# copy this file to ~/.ipython/profile_default/startup/
+def rich_init():
+    """
+    Doing this in a function keeps pollution into the global
+    namespace at a minimal level. 
+    """
+    from rich import pretty, traceback
+    pretty.install()
+    traceback.install()
+
+if __name__ == "__main__":
+    try:
+        rich_init()
+    except ImportError:
+        pass
