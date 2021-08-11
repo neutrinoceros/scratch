6b580b6ac27bfca744b84b0d27087a24d154937b
Clément Robert <cr52@protonmail.com>
2021-08-11 12:24:05 +0200

    fix retina setup in case matplotlib isn't installed

--- ipython/retina_init.py
+++ ipython/retina_init.py
@@ -1,4 +1,6 @@
 # install:
 # copy this file to ~/.ipython/profile_default/startup/
-from matplotlib_inline import backend_inline
-backend_inline.set_matplotlib_formats("retina")
+from importlib.util import find_spec
+if find_spec("matplotlib"):
+    from matplotlib_inline import backend_inline
+    backend_inline.set_matplotlib_formats("retina")
